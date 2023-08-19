e93cc6ae248a8170f202f9bccdc6736bec7b3129
divyakothuru311 <133106579+divyakothuru311@users.noreply.github.com>
2023-08-19 23:39:37 +0530

    Update providers.tf

--- nopterraform/providers.tf
+++ nopterraform/providers.tf
@@ -2,9 +2,9 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      #version = "5.13.0"
+      version = "5.13.0"
       #source  = "hashicorp/aws"
-      version = "3.74.0"
+      #version = "3.74.0"
     }
   }
 }
